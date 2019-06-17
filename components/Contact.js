25b97aa43a2c052d66eb2ff441876b3fda06de82
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-06-17 22:02:41 +0300

    Modify components js

--- components/Contact.js
+++ components/Contact.js
@@ -5,11 +5,11 @@ var Contact = React.createClass({
   
     render: function() {
       return (
-        <div className={'contactItem'}>
+        <div className='contactItem'>
           <img className='contactImage' src='http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'/>
-          <p className={'contactLabel'}> Imię: {this.props.item.firstName}</p>
-          <p className={'contactLabel'}> Nazwisko: {this.props.item.lastName}</p>
-          <a href={'mailto:' + this.props.item.email} className={'contactEmail'} value={this.props.item.email}> Link </a>
+          <p className='contactLabel'> Imię: {this.props.item.firstName}</p>
+          <p className='contactLabel'> Nazwisko: {this.props.item.lastName}</p>
+          <a href={'mailto:' + this.props.item.email} className='contactEmail' value={this.props.item.email}> Link </a>
         </div>
       )
     },
